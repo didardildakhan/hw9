f833428ca312d835f6df79d267fe73cbaf1baa76
Didar <didar.dildakhan.@narxoz.kz>
2026-05-04 10:23:43 +0500

    7

--- homework-rpg-10-main/src/com/narxoz/rpg/guild/Quartermaster.java
+++ homework-rpg-10-main/src/com/narxoz/rpg/guild/Quartermaster.java
@@ -1,21 +1,17 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for gear, supplies, and rewards.
- */
 public class Quartermaster extends GuildMember {
-
     public Quartermaster(String name, GuildMediator mediator) {
         super(name, mediator);
     }
 
     public void requestSupplies(String topic, String payload) {
-        // TODO: send a supply-related message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
-    @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        String sender = from == null ? "CouncilEngine" : from.getName();
+        System.out.println("[Quartermaster " + getName() + "] received " + topic + " from " + sender
+                + ": prepares equipment and budget for: " + payload);
     }
 }
