11f3b3a74301a186e1bea58e13c16bfc18377b5d
Didar <didar.dildakhan.@narxoz.kz>
2026-05-04 10:23:23 +0500

    6

--- homework-rpg-10-main/src/com/narxoz/rpg/guild/Captain.java
+++ homework-rpg-10-main/src/com/narxoz/rpg/guild/Captain.java
@@ -1,21 +1,17 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for orders and mission coordination.
- */
 public class Captain extends GuildMember {
-
     public Captain(String name, GuildMediator mediator) {
         super(name, mediator);
     }
 
     public void issueOrder(String topic, String payload) {
-        // TODO: send a command message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
-    @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        String sender = from == null ? "CouncilEngine" : from.getName();
+        System.out.println("[Captain " + getName() + "] received " + topic + " from " + sender
+                + ": command plan updated after message: " + payload);
     }
 }
