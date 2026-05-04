e38747dbe8e4b9d9f60e41549a6c4b5df37ded56
Didar <didar.dildakhan.@narxoz.kz>
2026-05-04 10:24:14 +0500

    8

--- homework-rpg-10-main/src/com/narxoz/rpg/guild/Scout.java
+++ homework-rpg-10-main/src/com/narxoz/rpg/guild/Scout.java
@@ -1,21 +1,17 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for route reports and reconnaissance.
- */
 public class Scout extends GuildMember {
-
     public Scout(String name, GuildMediator mediator) {
         super(name, mediator);
     }
 
     public void reportRoute(String topic, String payload) {
-        // TODO: send a scouting message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
-    @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        String sender = from == null ? "CouncilEngine" : from.getName();
+        System.out.println("[Scout " + getName() + "] received " + topic + " from " + sender
+                + ": checks route and enemy movement for: " + payload);
     }
 }
