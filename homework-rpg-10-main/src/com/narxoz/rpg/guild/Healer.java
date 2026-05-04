7d1ee552ac3b8609d4fa0e01b91efa1289ae6e2b
Didar <didar.dildakhan.@narxoz.kz>
2026-05-04 10:24:34 +0500

    9

--- homework-rpg-10-main/src/com/narxoz/rpg/guild/Healer.java
+++ homework-rpg-10-main/src/com/narxoz/rpg/guild/Healer.java
@@ -1,21 +1,17 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for wounds, potions, and recovery plans.
- */
 public class Healer extends GuildMember {
-
     public Healer(String name, GuildMediator mediator) {
         super(name, mediator);
     }
 
     public void prepareAid(String topic, String payload) {
-        // TODO: send a healing message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
-    @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        String sender = from == null ? "CouncilEngine" : from.getName();
+        System.out.println("[Healer " + getName() + "] received " + topic + " from " + sender
+                + ": prepares healing kits for: " + payload);
     }
 }
